fb3346ce66a2511417e805cde82a91e7c664fbdf
Raznan <muhammadraznan@satpaminternet.com>
2023-06-02 11:25:48 +0700

    push hello world

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include "vm.h"
 
+#include <string.h>
+
 int main(void) {
     ti_vm vm;
     ti_init_vm(&vm);
 
     uint8_t code[] = {
-        ASM_SET_R0_IMM, 2, 0, 0, 0, 0, 0, 0, 0,
+        ASM_SET_R0_IMM, 0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f,
         ASM_PUSH_R0,
+        ASM_SET_R0_IMM, 0x72, 0x6c, 0x64, 0x0, 0x0, 0x0, 0x0, 0x0,
         ASM_PUSH_R0
     };
 
